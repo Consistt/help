41eb642c832bab046547b58db6ff95cd5b547333
Consistt <126002840+Consistt@users.noreply.github.com>
2023-02-24 20:15:53 +0000

    Create cupid.cc

---
+++ cupid.cc
@@ -0,0 +1,10 @@
+local info = {
+  discord = "https://discord.gg/luabot";
+  status = "enabled";
+  announcement = "";
+  support_discord = {
+    strucid = "Consist#0001";
+    da_hood = "Consist#0001";
+  }
+}
+return info
